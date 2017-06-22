5786ed89bbb73d5a6d1403b3aea342eabfdaa4f0
yudha <developer09@esoftdream.net>
2017-06-22 11:44:36 +0700

    update sms page frontend 22 juni 2017

--- application/modules/page/views/page_contact_us_view.php
+++ application/modules/page/views/page_contact_us_view.php
@@ -16,59 +16,59 @@
 
             <div class="row">
                 <div class="col-md-8">
-                     
+
                     <form class="form-horizontal">
 
-                        <div class="panel panel-default">
-                                                   
-                                <div class="panel-body">                                                                        
-                                      Tinggalkan pesan Anda di sini. Kami akan membalasnya melalui E-Mail Anda.
-                                      <br>
-                                      <br>
-                                    <div class="form-group">
-                                        <label class="col-md-3 col-xs-12 control-label">Nama Anda</label>
-                                        <div class="col-md-6 col-xs-12">                                            
-                                            <div class="input-group">
-                                                <span class="input-group-addon"><span class="fa fa-pencil"></span></span>
-                                                <input class="form-control" type="text">
-                                            </div>                                            
-                                            <span class="help-block">Isikan dengan nama anda</span>
-                                        </div>
-                                    </div>
-                                    
-                                     <div class="form-group">
-                                        <label class="col-md-3 col-xs-12 control-label">Email Anda</label>
-                                        <div class="col-md-6 col-xs-12">                                            
-                                            <div class="input-group">
-                                                <span class="input-group-addon"><span class="fa fa-pencil"></span></span>
-                                                <input class="form-control" type="text">
-                                            </div>                                            
-                                            <span class="help-block">Isikan dengan email anda</span>
-                                        </div>
-                                    </div>
-                                    
-                                    <div class="form-group">
-                                        <label class="col-md-3 col-xs-12 control-label">Pesan</label>
-                                        <div class="col-md-6 col-xs-12">                                            
-                                            <textarea class="form-control" rows="5"></textarea>
-                                            <span class="help-block">Isikan dengan pesan anda</span>
-                                        </div>
-                                    </div>
-                           
-                                </div>
-                             <div class="col-md-9 col-xs-12">   
-                                                             
-                                    <button class="btn btn-primary pull-right">Submit</button>
-                             </div>
-                                    <br>
-                            </div>
-                            </form>
-                   
-                </div>
 
+
+                        <div class="panel-body">                                                                        
+                            Tinggalkan pesan Anda di sini. Kami akan membalasnya melalui E-Mail Anda.
+                            <br>
+                            <br>
+                            <div class="form-group">
+                                <label class="col-md-3 col-xs-12 control-label">Nama Anda</label>
+                                <div class="col-md-6 col-xs-12">                                            
+                                    <div class="input-group">
+                                        <span class="input-group-addon"><span class="fa fa-pencil"></span></span>
+                                        <input class="form-control" type="text">
+                                    </div>                                            
+                                    <span class="help-block">Isikan dengan nama anda</span>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-md-3 col-xs-12 control-label">Email Anda</label>
+                                <div class="col-md-6 col-xs-12">                                            
+                                    <div class="input-group">
+                                        <span class="input-group-addon"><span class="fa fa-pencil"></span></span>
+                                        <input class="form-control" type="text">
+                                    </div>                                            
+                                    <span class="help-block">Isikan dengan email anda</span>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-md-3 col-xs-12 control-label">Pesan</label>
+                                <div class="col-md-6 col-xs-12">                                            
+                                    <textarea class="form-control" rows="5"></textarea>
+                                    <span class="help-block">Isikan dengan pesan anda</span>
+                                </div>
+                            </div>
+                            <div class="col-md-9">
+                            <button class="btn btn-primary pull-right">Submit</button>
+                            </div>
+                        </div>
+
+
+
+                    </form>
+
+
+                </div>
+                <br>
             </div>
 
 
         </div>
-
+    </div>
     </div>
--- application/modules/page/views/page_view.php
+++ application/modules/page/views/page_view.php
@@ -1,64 +1,55 @@
 <div class="col-md-12">
     <div class="panel-heading">
         <div class="panel-title-box">
-            <h3>Tentang Kami</h3>
+            <h3>SMS Pengaduan</h3>
         </div>                                    
     </div>  
 </div>
 <div class="col-md-12">
-
-    <!-- START USERS ACTIVITY BLOCK -->
     <div class="panel panel-default">
-
-
-
         <div class="page-content-wrap">
+            <div class="row">
+                <br>
+                <?php
+                if (!empty($query)) {
 
+                    foreach ($query as $row) {
+                        if (!empty($row->content)) {
+                            $sender = $row->sender;
+                            $count = strlen($sender);
+                            $jumlah = $count - 3;
+                            $number = substr($sender, $jumlah, 3);
+                            $sender_replace = str_replace($number, 'xxx', $sender);
+                            $date = substr($row->in_datetime, 0, 10);
+                            $time = substr($row->in_datetime, 11, 8);
 
+                            $text = substr($row->content, 0, 70);
+                            ?>
 
-            <?php
-            if (!empty($query)) {
-
-                foreach ($query as $row) {
-                    if (!empty($row->content)) {
-                        $sender = $row->sender;
-                        $count = strlen($sender);
-                        $jumlah = $count - 3;
-                        $number = substr($sender, $jumlah, 3);
-                        $sender_replace = str_replace($number, 'xxx', $sender);
-                        $date = substr($row->in_datetime, 0, 10);
-                        $time = substr($row->in_datetime, 11, 8);
-
-                        $text = substr($row->content, 0, 50);
-                        ?>
-                        <div class="panel-body">
-
-                            <div class="messages messages-img">
-                                <div class="item item-visible">
-                                    <div class="image">
+                            <div class="media">
+                                <div class="pull-left">
+                                    <span class="fa-stack fa-2x" style="margin-left: 25px;">
                                         <img src="<?php echo $themes_url; ?>img\user\human.jpg" alt="">
-                                    </div>                                
-                                    <div class="text">
-                                        <div class="heading">
-                                            <a href="#"><?php echo $sender_replace; ?></a>
-                                            <span class="date"><?php echo $date; ?> - <?php echo $time . ' WIT'; ?></span>  
-                                        </div>                                    
-                                        <?php echo $text . '...'; ?>
-                                    </div>
+                                    </span> 
                                 </div>
-
+                                <div class="media-body">
+                                    <h4 class="media-heading"><?php echo $sender_replace; ?></h4>
+                                    <span class="date"><?php echo $date; ?> - <?php echo $time . ' WIT'; ?></span> 
+                                    <p><?php echo $text . '...'; ?></p>
+                                </div>
                             </div>
-                        </div>
-                        <?php
-                    } else {
-                        echo "Data sms belum ada";
-                        ?>
-                        <?php
+                            <hr>
+
+                            <?php
+                        } else {
+                            echo "Data sms belum ada";
+                            ?>
+                            <?php
+                        }
                     }
                 }
-            }
-            ?>
-
+                ?>
+            </div>
         </div>
     </div>
 </div>
